7ea8f0876cd0464d016175c508b76283fbe6639d
gab <gabriel.pontarolo045@gmail.com>
2023-06-04 17:41:43 -0300

    best result yet

--- cartas/cartas.py
+++ cartas/cartas.py
@@ -5,9 +5,11 @@ from sys import argv
 from os import listdir
 from scipy.signal import find_peaks
 
-DIR_NAME = "tr"
+DIR_NAME = "vl"
 ACCEPTED_FORMATS = ["png", "jpg"]
 RESIZE_FACTOR = 4 
+DISTANCE = 20.5 # valor encontrado empiricamente
+HEIGHT_FACTOR = 7
 
 def correct_skew(img, delta=1, limit=5):
     """
@@ -41,9 +43,8 @@ def pre_processing(img):
     img = cv2.resize(img, (ow//RESIZE_FACTOR, oh//RESIZE_FACTOR), interpolation=cv2.INTER_CUBIC)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.blur(gray, (3, 3))
 
-    _, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    _, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     
     return thresh
 
@@ -64,26 +65,12 @@ def centralize_letter(img):
 
 def line_dilatation(img):
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (20, 1))
-    closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
-    opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-    diff = cv2.absdiff(opening, closing)
-    
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-    closing = cv2.morphologyEx(diff, cv2.MORPH_CLOSE, kernel)
-
-    return closing
-
-    # aumenta a largura das linhas juntando as palavras
-    hor_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 1), anchor=(9, 0))
-
-    # diminui a altura das linhas
+    hor_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 2))
     mask = cv2.dilate(img, hor_kernel, iterations=1)
-    kernel = np.ones((5,5), np.uint8)
-
+    kernel = np.ones((5, 5), np.uint8)
     return cv2.erode(mask, kernel, iterations=1)
 
-def count_lines(img_paths):
+def count_lines(img_paths, should_show=False):
 
     correct_imgs = 0
     for img_path in img_paths:
@@ -103,48 +90,35 @@ def count_lines(img_paths):
         crop = centralize_letter(rotated)
 
         dilatated = line_dilatation(crop)
+        (h, w) = dilatated.shape[:2]
 
         # calcula linhas pelos picos da projecao vertical
         projection = np.sum(dilatated, axis=1, dtype=np.uint32) / 255
-        projection[projection < 10] = 0
 
-        # encontra a distancia media esperada entre os picos
-        (h, w) = dilatated.shape[:2]
-        countors, _ = cv2.findContours(dilatated[0:h, 0:w//5], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        moments = [cv2.moments(c) for c in countors]
-        centroids = [(int(m["m10"] / m["m00"]), int(m["m01"] / m["m00"])) for m in moments]
-        centroids.sort(key=lambda x: x[1])
-        mean_dist = np.mean([centroids[i+1][1] - centroids[i][1] for i in range(len(centroids)-1)])
-        peaks = find_peaks(projection, distance=mean_dist, height=10)
-        lines_num = len(peaks[0])
-
-        d = cv2.cvtColor(dilatated[0:h, 0:w//5], cv2.COLOR_GRAY2BGR)
-        for c in centroids:
-            cv2.circle(d, c, 2, (0, 0, 255), 2)
-
-        cv2.imshow("d", d)
-        cv2.waitKey(0)
+        peaks, _ = find_peaks(projection, height=np.average(projection)//HEIGHT_FACTOR, distance=DISTANCE)
+        lines_num = len(peaks)
+        #print(len(projection))
 
         if lines_num == actual_line_num:
             correct_imgs += 1
         print(f"{img_name} {actual_line_num}  {lines_num}")
 
-        (h, w) = dilatated.shape[:2]
-        projection_img = np.zeros((projection.shape[0], w, 3), dtype=np.uint8)
-        for i in range(len(projection)):
-            cv2.line(projection_img, (0, i), (int(projection[i]), i), (255, 255, 255), 1)
-        
-        for i in peaks[0]:
-            cv2.line(projection_img, (0, i), (int(projection[i]), i), (0, 0, 255), 1)
-        
-        # mask_out = cv2.cvtColor(dilatated, cv2.COLOR_GRAY2BGR)
-        # show = np.concatenate([mask_out, projection_img], axis=1)
-        # cv2.imshow("img", show)
-        # cv2.waitKey(0)
+        if should_show:
+            projection_img = np.zeros((projection.shape[0], w, 3), dtype=np.uint8)
+            for i in range(len(projection)):
+                cv2.line(projection_img, (0, i), (int(projection[i]), i), (255, 255, 255), 1)
+            
+            for i in peaks:
+                cv2.line(projection_img, (0, i), (int(projection[i]), i), (0, 0, 255), 1)
+            
+            mask_out = cv2.cvtColor(dilatated, cv2.COLOR_GRAY2BGR)
+            show = np.concatenate([mask_out, projection_img], axis=1)
+            cv2.imshow("img", show)
+            cv2.waitKey(0)
 
     print(f"Cartas corretas: {correct_imgs} {len(img_paths)}")
 
-def count_words(img_paths):
+def count_words(img_paths, should_show=False):
     for img_path in img_paths:
         img_name = img_path.split("/")[-1]
 
@@ -179,8 +153,6 @@ def main():
         print("Uso: python3 " + argv[0] + " <modo: -l ou -w>")
         exit(1)
 
-    mode = argv[1]
-
     # caminho das imagens
     img_paths = [f"{DIR_NAME}/{f}" for f in listdir(DIR_NAME) if f.split(".")[-1] in ACCEPTED_FORMATS]
     img_paths.sort()
@@ -190,10 +162,12 @@ def main():
     else:
         print(f"{len(img_paths)} imagens encontradas")
 
-    if mode == "-l":
-        count_lines(img_paths)
-    elif mode == "-w":
-        count_words(img_paths)
+    should_show = "-s" in argv
+
+    if "-l" in argv:
+        count_lines(img_paths, should_show)
+    elif "-w" in argv:
+        count_words(img_paths, should_show)
     else:
         print("Modo inválido")
 
